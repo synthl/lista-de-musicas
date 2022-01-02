78cb57b3afb0e96115532a45cad7b1003d7d17e8
Synth_L <leonardoantoniopinto12@gmail.com>
2022-01-02 20:23:22 -0300

    Projeto criado

---
+++ js/musics.js
@@ -0,0 +1,13 @@
+/* function filtrarTom(){
+    var select = window.document.getElementById('tom_filter');
+    var tons = window.document.getElementsByClassName('tom');
+
+    if (select.value == 1){
+        tons.sort(["a,b"])
+    }
+}
+
+
+    //    var tom = window.document.getElementByClass('tom').val();
+  var lines = tom.split("\n"); */
+
